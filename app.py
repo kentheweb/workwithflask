e8976068fa0fffbaf8c259b17cce96368e1d4712
kentheweb <kenthewebmaster@gmail.com>
2020-06-07 16:22:43 +0100

    worked with some other files

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
-from flask import Flask, render_template, request, redirect, url_for, session, jsonify, make_response
-from flask_socketio import SocketIO, join_room
+from flask import Flask, render_template, request, redirect, url_for, session, jsonify, make_response, flash
+from flask_socketio import SocketIO, join_room, leave_room
 from flask_sqlalchemy import SQLAlchemy
 from werkzeug.security import generate_password_hash, check_password_hash
 import hashlib
@@ -8,7 +8,7 @@ app = Flask(__name__)
 
 
 app.config['SECRET_KEY'] = 'LUMULI'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////app.sqlite'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.sqlite'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 app.config['SQLALCHEMY_ECHO'] = True
 
@@ -21,21 +21,32 @@ class User(db.Model):
     name = db.Column(db.String(200), nullable=False)
     email = db.Column(db.String(200), nullable=False)
     hash = db.Column(db.String(200), nullable=False)
+    password = db.Column(db.String(200), nullable=False)
+
+    def __repr__(self):
+        return 'name %s' % self.name
+
+
+class Group(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(250), nullable=False)
+    description = db.Column(db.Text, nullable=False)
 
     def __repr__(self):
         return 'name %s' % self.name
 
 
 def generate_hash(email):
-    url = hashlib.md5(email).hexdigest()
+    url = hashlib.md5(str(email).encode('utf-8')).hexdigest()
     return url
 
 
 @app.route('/home')
 def home():
     if 'email' and 'username' in session:
+        user = User.query.filter_by().all()
         name = session['username']
-        return render_template('index.html', name=name)
+        return render_template('index.html', name=name, user=user)
     else:
         return redirect(url_for('login'))
 
@@ -47,10 +58,39 @@ def login():
     else:
         email = request.form.get('email')
         password = request.form.get('password')
-        username = request.form.get('username')
-        session['email'] = email
-        session['username'] = username
-        return redirect(url_for('home'))
+        user = User.query.filter_by(email=email).first()
+        if user:
+            if check_password_hash(user.password, password=password):
+                session['email'] = email
+                session['username'] = user.name
+                return redirect(url_for('home'))
+            else:
+                flash('wrong password the field may be too sensitive', 'danger')
+                return redirect(url_for('login'))
+        else:
+            flash('please create an account before login in', 'danger')
+            return redirect(url_for('register'))
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if request.method == 'POST':
+        name = request.form.get('name')
+        email = request.form.get('email')
+        hash = generate_hash(email=email)
+        password = request.form.get('password')
+        passw = generate_password_hash(password=password, method='sha256')
+        user = User.query.filter_by(email=email).first()
+        if user:
+            flash('the email entered already exists please choose a different one', 'danger')
+            return redirect(url_for('register'))
+        user = User(name=name, email=email, password=passw, hash=hash)
+        db.session.add(user)
+        db.session.commit()
+        flash('account created successfully', 'success')
+        return redirect(url_for('login'))
+    else:
+        return render_template('register.html')
 
 
 @app.route('/logout')
@@ -71,9 +111,10 @@ def register_group():
     }))
 
 
-@app.route('/start/conversation/<name>')
-def converse(name):
-    details = name
+@app.route('/start/conversation/<int:id>')
+def converse(id):
+    user = User.query.get_or_404(id)
+    details = user.name
     identify = session['username']
     return render_template('converse.html', details=details, identify=identify)
 
@@ -82,6 +123,16 @@ def converse(name):
 def search():
     name = request.args.get('search')
     return name
+
+
+@app.route('/account')
+def account():
+    if 'email in session':
+        user = session['email']
+        client = User.query.filter_by(email=user).first()
+        return render_template('account.html', client=client)
+
+    return redirect(url_for('login'))
 
 
 @socketio.on('my event')
